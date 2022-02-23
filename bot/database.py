769107b59d5d615bd716bb6424b421e0eb1fa3cb
Pritam Dey <pdeypritam@gmail.com>
2022-02-23 20:01:22 +0530

    added indian local time

--- bot/database.py
+++ bot/database.py
@@ -1,15 +1,19 @@
+import datetime as dt
 from pathlib import Path
 
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import Column, DateTime, MetaData, create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 
 Base = automap_base()
+indian_time = lambda: dt.datetime.utcnow() + dt.timedelta(hours=5, minutes=30)
 
 
 # customise models
 class Note(Base):  # type: ignore
     __tablename__ = "notes"
+
+    date_time = Column(DateTime, default=indian_time)
 
     def __repr__(self) -> str:
         """Represent self."""
